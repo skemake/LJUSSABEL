0e3fb9263f911e52de774a43576a2223eeadd203
Anders Sandstrom <anders@sredna.se>
2017-04-06 18:19:49 +0200

    Added check of busy pin
    First sample played will be the "lockup" sound
    If the busy pin is working this sound will play for a couple of seconds
    If busy pin is faulty the lockup sound will only play for 500ms

--- Software/LJUDTEST/LJUDTEST.ino
+++ Software/LJUDTEST/LJUDTEST.ino
@@ -1,3 +1,4 @@
+
 #include "OneLineWT588D.h"
 
 // Sound chip
@@ -7,16 +8,34 @@
 
 OneLineWT588D Soundchip(WT588D_DATA_PIN,WT588D_BUSY_PIN,WT588D_RESET_PIN);
 
+bool readyOk = false;
+
 void setup()
 {
   Soundchip.warmUp();
 }
 
+void checkReadyOk()
+{
+  if(!readyOk)
+  {
+    delay(500);
+    Soundchip.play(0x2c); //"lockup" sound
+    delay(500);
+    while(Soundchip.isBusy() == true)
+    {
+      delay(10);
+    }
+    readyOk = true;
+  }
+}
 void loop()
 {
- for(int i = 0;i<=51;i++)
- {
-  Soundchip.play(i);
-  delay(1000);
- }
+  checkReadyOk();
+  for(int i = 0;i<=51;i++)
+  {
+    Soundchip.play(i);
+    delay(1000);
+  }
 }
+
